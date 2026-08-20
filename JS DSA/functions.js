e0261f10db71bd743329d4d5693cd161449a900a
SandeepOjha1 <sandeepojha028@gmail.com>
2026-08-20 19:36:58 +0530

    DSA

--- JS DSA/functions.js
+++ JS DSA/functions.js
@@ -1,23 +1,23 @@
 //Basic of function
-function hello(){
+function hello() {
     console.log("hello");
 }
 hello();
 
 //function for loop
-function loop(){
-    for(let i=1; i <=5; i++){
-        console.log("Loop from 1 to 5: ",i);
+function loop() {
+    for (let i = 1; i <= 5; i++) {
+        console.log("Loop from 1 to 5: ", i);
     }
 }
 loop();
 
 // function for if-else statements
-function statements(){
+function statements() {
     let age = 19;
-    if(age >= 18){
+    if (age >= 18) {
         console.log("Adult");
-    }else{
+    } else {
         console.log("Not adult");
     }
 }
@@ -25,7 +25,7 @@ function statements(){
 statements();
 
 // create a function to print a poem
-function poem(){
+function poem() {
     console.log("Twinkle Twinkle little star...");
     console.log("Jhonny Jhonny");
 }
@@ -34,7 +34,7 @@ poem();
 
 //Create a function to roll a dice and always displays the value of the dice(0 to 6).
 
-function roll(){
+function roll() {
     let math = Math.floor(Math.random() * 6) + 1;
     console.log("Roll dice: ", math);
 }
@@ -43,7 +43,7 @@ roll();
 
 // Functions with arguments
 
-function Mname(name,age){
+function Mname(name, age) {
     console.log(`${name}'s age is ${age}`);
 }
 
@@ -51,8 +51,8 @@ Mname("Sandeep", 21);
 Mname(23);
 
 // Create a function that gives the average of 3 numbers
-function average(a,b,c){
-    let averagenumb = (a + b + c)/3;
+function average(a, b, c) {
+    let averagenumb = (a + b + c) / 3;
     console.log("Average of 3 numbers: ", averagenumb);
 }
-average(1,5,9);
+average(1, 5, 9);
