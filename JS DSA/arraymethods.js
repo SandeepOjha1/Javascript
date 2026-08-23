7018e2fe31e9154668cf95b4babfc2c181f17d91
SandeepOjha1 <sandeepojha028@gmail.com>
2026-08-23 20:31:32 +0530

    DSA

--- JS DSA/arraymethods.js
+++ JS DSA/arraymethods.js
@@ -1,54 +1,49 @@
 // forEach Method
 
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 
-function print(el){
-    console.log(el);
+function print(el) {
+  console.log(el);
 }
 
 // arr.forEach(print);
 
 // OR
 
-arr.forEach(function(el)
-{
-    console.log(el);
-})
-
+arr.forEach(function (el) {
+  console.log(el);
+});
 
 // Map Method
 
-let num = [1,2,3,4];
+let num = [1, 2, 3, 4];
 
 let double = num.map((el) => {
-    return el * el;
-    })
-    console.log(double);
+  return el * el;
+});
+console.log(double);
 
 // Filter Method
 
-let numb = [1,2,3,4,5,6,7,8,9];
-let evens = numb.filter(( numb) => (numb % 2 == 0));
+let numb = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let evens = numb.filter((numb) => numb % 2 == 0);
 console.log(evens);
 
-// Reduce 
+// Reduce
 
-let a = [4,5,9,52,10,2,3];
+let a = [4, 5, 9, 52, 10, 2, 3];
 
-let finalval = a.reduce((res, el) => res+el);
+let finalval = a.reduce((res, el) => res + el);
 console.log(finalval);
 
-
 // Spread
-let arrr = [1,2,3,4,5];
+let arrr = [1, 2, 3, 4, 5];
 let newarr = [...arrr];
 
 let chars = [..."Helooooooooo"];
 
-let odd = [1,3,5,7,9];
-let even = [0,2,4,6,8];
+let odd = [1, 3, 5, 7, 9];
+let even = [0, 2, 4, 6, 8];
 
-let numss = [...even, ...odd];/// ... here 3 dots are spread function
+let numss = [...even, ...odd]; /// ... here 3 dots are spread function
 console.log(numss);
-
-
