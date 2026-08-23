341de6c8a6c6c56b57b92bc13780a6f39d0a8fe0
SandeepOjha1 <sandeepojha028@gmail.com>
2026-08-23 20:31:00 +0530

    DSA

--- JS DSA/arraymethods.js
+++ JS DSA/arraymethods.js
@@ -28,8 +28,8 @@ let double = num.map((el) => {
 // Filter Method
 
 let numb = [1,2,3,4,5,6,7,8,9];
-let even = numb.filter(( numb) => (numb % 2 == 0));
-console.log(even);
+let evens = numb.filter(( numb) => (numb % 2 == 0));
+console.log(evens);
 
 // Reduce 
 
@@ -37,3 +37,18 @@ let a = [4,5,9,52,10,2,3];
 
 let finalval = a.reduce((res, el) => res+el);
 console.log(finalval);
+
+
+// Spread
+let arrr = [1,2,3,4,5];
+let newarr = [...arrr];
+
+let chars = [..."Helooooooooo"];
+
+let odd = [1,3,5,7,9];
+let even = [0,2,4,6,8];
+
+let numss = [...even, ...odd];/// ... here 3 dots are spread function
+console.log(numss);
+
+
