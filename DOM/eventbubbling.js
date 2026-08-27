07a20c44fc1863c677773c98afb497597b0d7dfa
SandeepOjha1 <sandeepojha028@gmail.com>
2026-08-27 14:06:54 +0530

    DOM

---
+++ DOM/eventbubbling.js
@@ -0,0 +1,17 @@
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let li = document.querySelectorAll("li");   
+
+div.addEventListener("click", function(){
+    console.log("div was clicked");    
+});
+
+ul.addEventListener("click", function(){
+    console.log("ul was clicked");    
+});
+
+for(li of li){
+li.addEventListener("click", function(){
+    console.log("li was clicked");    
+    })
+}
